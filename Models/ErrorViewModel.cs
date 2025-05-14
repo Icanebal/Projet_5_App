ac8ba1a462a7fd05598e011abf0e16bfcbe1a3c7
Icanebal <arthurlouismarteau@gmail.com>
2025-05-14 19:16:35 +0200

    New Solution

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Projet_5.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
